ef8e231a67d138a91a8c6c37cd1dc23e01daf86b
Yann@SeenKid <30565751+SeenKid@users.noreply.github.com>
2023-09-08 11:19:29 +0200

    remove RSA part

--- hash.py
+++ hash.py
@@ -13,20 +13,3 @@ print(result2 + "\n")
 # hash some text with SHA1
 result3 = hashlib.sha1(b'TextToHash').hexdigest()
 print(result3 + "\n")
-
-# generate a key
-key = RSA.generate(1024)
-print(key)
-
-# export keys
-k = key.exportKey('PEM')
-p = key.publickey().exportKey('PEM')
-
-# open files
-with open('private.pem','w') as kf:
-	kf.write(k.decode())
-	kf.close()
-
-with open('public.pem','w') as pf:
-	pf.write(p.decode())
-	pf.close()
